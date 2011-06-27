2e3b476e0a24ae770d555626ff750b2004bbd489
Zhihao Yuan <lichray@gmail.com>
2011-06-27 01:05:56 -0500

    Includes key.o and multibyte.h

---
+++ common/multibyte.h
@@ -0,0 +1,67 @@
+#ifndef MULTIBYTE_H
+#define MULTIBYTE_H
+
+/*
+ * Fundamental character types.
+ *
+ * CHAR_T	An integral type that can hold any character.
+ * ARG_CHAR_T	The type of a CHAR_T when passed as an argument using
+ *		traditional promotion rules.  It should also be able
+ *		to be compared against any CHAR_T for equality without
+ *		problems.
+ * MAX_CHAR_T	The maximum value of any character.
+ *
+ * If no integral type can hold a character, don't even try the port.
+ */
+typedef	u_int		ARG_CHAR_T;
+
+#ifdef USE_WIDECHAR
+#include <wchar.h>
+#include <wctype.h>
+
+typedef	wchar_t		RCHAR_T;
+#define RCHAR_T_MAX	((1 << 24)-1)
+typedef	wchar_t		CHAR_T;
+#define	MAX_CHAR_T	0xffffff    /* XXXX */
+typedef	u_int		UCHAR_T;
+#define RCHAR_BIT	24
+
+#define STRLEN		wcslen
+#define STRTOL		wcstol
+#define STRTOUL		wcstoul
+#define SPRINTF		swprintf
+#define STRCMP		wcscmp
+#define STRPBRK		wcspbrk
+#define TOUPPER		towupper
+#define STRSET		wmemset
+
+#define L(ch)		L ## ch
+
+#else
+typedef	char		RCHAR_T;
+#define RCHAR_T_MAX	CHAR_MAX
+typedef	u_char		CHAR_T;
+#define	MAX_CHAR_T	0xff
+typedef	u_char		UCHAR_T;
+#define RCHAR_BIT	CHAR_BIT
+
+#define STRLEN		strlen
+#define STRTOL		strtol
+#define STRTOUL		strtoul
+#define SPRINTF		snprintf
+#define STRCMP		strcmp
+#define STRPBRK		strpbrk
+#define TOUPPER		toupper
+#define STRSET		memset
+
+#define L(ch)		ch
+
+#endif
+
+#define MEMCMP(to, from, n) 						    \
+	memcmp(to, from, (n) * sizeof(*(to)))
+#define	MEMMOVE(p, t, len)	memmove(p, t, (len) * sizeof(*(p)))
+#define	MEMCPY(p, t, len)	memcpy(p, t, (len) * sizeof(*(p)))
+#define SIZE(w)		(sizeof(w)/sizeof(*w))
+
+#endif
