cb94f4b8575e6b4e5cde007b6c5cfe04448c4d1c
Zhihao Yuan <lichray@gmail.com>
2011-11-16 15:22:15 -0600

    Fixes memory leak when sourcing script.

--- ex/ex_source.c
+++ ex/ex_source.c
@@ -42,7 +42,8 @@ ex_source(SCR *sp, EXCMD *cmdp)
 	char *bp;
 	char *name;
 	size_t nlen;
-	CHAR_T *wp, *dp;
+	CHAR_T *wp;
+	static CHAR_T *dp = NULL;
 	size_t wlen;
 
 	INT2CHAR(sp, cmdp->argv[0]->bp, cmdp->argv[0]->len + 1, name, nlen);
@@ -84,6 +85,7 @@ err:		msgq_str(sp, M_SYSERR, name, "%s");
 
 	if (CHAR2INT(sp, bp, (size_t)sb.st_size + 1, wp, wlen))
 		msgq(sp, M_ERR, "323|Invalid input. Truncated.");
+	if (dp) free(dp);
 	dp = v_wstrdup(sp, wp, wlen - 1);
 	free(bp);
 	/* Put it on the ex queue. */
