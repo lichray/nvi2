5ea4ff6c9d13f3cd4acf43cad2b603a81ffb3dc7
Zhihao Yuan <lichray@gmail.com>
2012-04-12 05:30:19 -0500

    An && bug influenced by b59c2c2.

--- ex/ex_cd.c
+++ ex/ex_cd.c
@@ -91,7 +91,7 @@ ex_cd(SCR *sp, EXCMD *cmdp)
 	 */
 	if (cmdp->argc == 0 ||
 	    (ap = cmdp->argv[0])->bp[0] == '/' ||
-	    (ap->len == 1 && ap->bp[0]) == '.' ||
+	    (ap->len == 1 && ap->bp[0] == '.') ||
 	    (ap->len >= 2 && ap->bp[0] == '.' && ap->bp[1] == '.' &&
 	    (ap->bp[2] == '/' || ap->bp[2] == '\0')))
 		goto err;
