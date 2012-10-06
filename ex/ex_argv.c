lichray/nvi2

commit b981de196b3def0b33c4e765634048fdbf350e55

Support ~user expansion in file completion.

ex/ex_argv.c
@@ -513,7 +513,7 @@ argv_flt_path(SCR *sp, EXCMD *excp, CHAR_T *path, size_t plen)
 	size_t dlen, len, nlen;
 	CHAR_T *dname;
 	CHAR_T *p, *np, *n;
-	char *name, *tp;
+	char *name, *tp, *epd = NULL;
 	CHAR_T *wp;
 	size_t wlen;
 
@@ -531,7 +531,6 @@ argv_flt_path(SCR *sp, EXCMD *excp, CHAR_T *path, size_t plen)
 			dname = L("/");
 			dlen = 1;
 		} else {
-			*p = '\0';
 			dname = path;
 			dlen = p - path;
 		}
@@ -539,10 +538,20 @@ argv_flt_path(SCR *sp, EXCMD *excp, CHAR_T *path, size_t plen)
 	}
 
 	INT2CHAR(sp, dname, dlen + 1, tp, nlen);
+	if (*tp == '~') {
+		if ((epd = expanduser(tp)) == NULL) {
+			free(path);
+			return (0);
+		}
+		tp = epd;
+	}
 	if ((dirp = opendir(tp)) == NULL) {
 		msgq_str(sp, M_SYSERR, tp, "%s");
+		free(epd);
+		free(path);
 		return (1);
 	}
+	free(epd);
 
 	INT2CHAR(sp, np, STRLEN(np), tp, nlen);
 	if ((name = v_strdup(sp, tp, nlen)) == NULL) {
@@ -778,10 +787,19 @@ argv_esc(SCR *sp, EXCMD *excp, CHAR_T *str, size_t len)
 		    IS_ESCAPE(sp, excp, ch))			/* Ex. */
 			*p++ = CH_LITERAL;
 		else switch (ch) {
-		case '!': case '%': case '#':			/* Ex exp. */
-		case '+':					/* Ex +cmd. */
-			if (p != bp || ch != '+')
+		case '~':					/* ~user. */
+			if (p != bp)
 				*p++ = '\\';
+			break;
+		case '+':					/* Ex +cmd. */
+			if (p == bp)
+				*p++ = '\\';
+			break;
+		case '%':					/* Ex exp. */
+			*p++ = '\\';
+			break;
+		case '!': case '#':
+			*p++ = '\\';
 			if (IS_SHELLMETA(sp, ch))
 				*p++ = '\\';
 			break;

vi/v_txt.c
@@ -1998,7 +1998,7 @@ txt_fc(SCR *sp, TEXT *tp, int *redrawp)
 	size_t indx, len, nlen, off;
 	int argc;
 	CHAR_T *p, *t, *bp;
-	char *np;
+	char *np, *epd = NULL;
 	size_t nplen;
 	int fstwd = 1;
 
@@ -2113,6 +2113,11 @@ txt_fc(SCR *sp, TEXT *tp, int *redrawp)
 
 	/* If a single match and it's a directory, append a '/'. */
 	INT2CHAR(sp, cmd.argv[0]->bp, cmd.argv[0]->len + 1, np, nplen);
+	if (*np == '~') {
+		if ((epd = expanduser(np)) == NULL)
+			return (1);
+		np = epd;
+	}
 	if (!stat(np, &sb) && S_ISDIR(sb.st_mode)) {
 		if (tp->owrite == 0) {
 			off = p - tp->lb;
@@ -2127,6 +2132,7 @@ txt_fc(SCR *sp, TEXT *tp, int *redrawp)
 		++tp->cno;
 		*p++ = '/';
 	}
+	free(epd);
 	return (0);
 }
 

commit 107196eac1358ecb7203719b01fedae1f25178f0

Fix a bug introduced by b981de1.
NUL must be put in a 8-bit build.

ex/ex_argv.c
@@ -531,6 +531,7 @@ argv_flt_path(SCR *sp, EXCMD *excp, CHAR_T *path, size_t plen)
 			dname = L("/");
 			dlen = 1;
 		} else {
+			*p = '\0';
 			dname = path;
 			dlen = p - path;
 		}
@@ -571,7 +572,8 @@ argv_flt_path(SCR *sp, EXCMD *excp, CHAR_T *path, size_t plen)
 		}
 
 		/* Directory + name + slash + null. */
-		argv_alloc(sp, dlen + len + 2);
+		CHAR2INT(sp, dp->d_name, len + 1, wp, wlen);
+		argv_alloc(sp, dlen + wlen + 1);
 		n = exp->args[exp->argsoff]->bp;
 		if (dlen != 0) {
 			MEMCPY(n, dname, dlen);
@@ -580,7 +582,6 @@ argv_flt_path(SCR *sp, EXCMD *excp, CHAR_T *path, size_t plen)
 				*n++ = '/';
 			exp->args[exp->argsoff]->len = dlen + 1;
 		}
-		CHAR2INT(sp, dp->d_name, len + 1, wp, wlen);
 		MEMCPY(n, wp, wlen);
 		exp->args[exp->argsoff]->len += wlen - 1;
 		++exp->argsoff;