lichray/nvi2

commit 3a0ec5141407122bb04d9f1fda0756c2e145312a

Redefine MEMMOVE to wmemmove(3) under DEBUG mode.
And re-enable the old bcopy(3) and bzero(3).

common/log.c
@@ -277,7 +277,7 @@ log_line(
 	    len * sizeof(CHAR_T) + CHAR_T_OFFSET);
 	ep->l_lp[0] = action;
 	memmove(ep->l_lp + sizeof(u_char), &lno, sizeof(recno_t));
-	MEMMOVEW(ep->l_lp + CHAR_T_OFFSET, lp, len);
+	memmove(ep->l_lp + CHAR_T_OFFSET, lp, len);
 
 	lcur = ep->l_cur;
 	key.data = &lcur;

common/conv.c
@@ -115,7 +115,7 @@ default_char2int(SCR *sp, const char * str, ssize_t len, CONVWIN *cw,
     size_t	left = len;
     int		error = 1;
 
-    MEMSET(&mbs, 0, 1);
+    BZERO(&mbs, 1);
     BINC_RETW(NULL, *tostr, *blen, nlen);
 
 #ifdef USE_ICONV
@@ -243,7 +243,7 @@ default_int2char(SCR *sp, const CHAR_T * str, ssize_t len, CONVWIN *cw,
 #endif
 
 
-    MEMSET(&mbs, 0, 1);
+    BZERO(&mbs, 1);
     BINC_RETC(NULL, *tostr, *blen, nlen);
     dst = *tostr; buflen = *blen;
 
@@ -319,7 +319,7 @@ conv_init(SCR *orig, SCR *sp)
     if (orig == NULL)
 	setlocale(LC_ALL, "");
     if (orig != NULL)
-	MEMMOVE(&sp->conv, &orig->conv, 1);
+	BCOPY(&orig->conv, &sp->conv, 1);
 #ifdef USE_WIDECHAR
     else {
 	char *ctype, *cp;

common/key.c
@@ -23,6 +23,7 @@ static const char sccsid[] = "$Id: key.c,v 10.53 2012/02/13 19:22:25 zy Exp $";
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 #include <unistd.h>
 
 #include "common.h"
@@ -382,8 +383,8 @@ v_event_push(
 	if (total >= gp->i_nelem && v_event_grow(sp, MAX(total, 64)))
 		return (1);
 	if (gp->i_cnt)
-		MEMMOVE(gp->i_event + TERM_PUSH_SHIFT + nitems,
-		    gp->i_event + gp->i_next, gp->i_cnt);
+		BCOPY(gp->i_event + gp->i_next,
+		    gp->i_event + TERM_PUSH_SHIFT + nitems, gp->i_cnt);
 	gp->i_next = TERM_PUSH_SHIFT;
 
 	/* Put the new items into the queue. */

common/key.h
@@ -6,7 +6,7 @@
  *
  * See the LICENSE file for redistribution information.
  *
- *	$Id: key.h,v 10.54 2012/02/13 19:43:08 zy Exp $
+ *	$Id: key.h,v 10.55 2012/10/07 01:31:17 zy Exp $
  */
 
 #include "multibyte.h"
@@ -44,9 +44,6 @@
     FILE2INT5(sp,sp->cw,n,nlen,w,wlen)
 #define CHAR2INT(sp,n,nlen,w,wlen)					    \
     CHAR2INT5(sp,sp->cw,n,nlen,w,wlen)
-
-#define MEMMOVEW(to, from, n) \
-    memmove(to, from, (n) * sizeof(CHAR_T))
 
 /* The maximum number of columns any character can take up on a screen. */
 #define	MAX_CHARACTER_COLUMNS	7

common/mem.h
@@ -6,7 +6,7 @@
  *
  * See the LICENSE file for redistribution information.
  *
- *	$Id: mem.h,v 10.16 2012/04/14 03:51:52 zy Exp $
+ *	$Id: mem.h,v 10.17 2012/10/07 00:40:29 zy Exp $
  */
 
 #ifdef DEBUG
@@ -207,11 +207,11 @@
 }
 
 /*
- * Versions of memmove(3) and memset(3) that use the size of the
+ * Versions of bcopy(3) and bzero(3) that use the size of the
  * initial pointer to figure out how much memory to manipulate.
  */
-#define	MEMMOVE(p, t, len)	memmove(p, t, (len) * sizeof(*(p)))
-#define	MEMSET(p, value, len)	memset(p, value, (len) * sizeof(*(p)))
+#define	BCOPY(p, t, len)	bcopy(p, t, (len) * sizeof(*(p)))
+#define	BZERO(p, len)		bzero(p, (len) * sizeof(*(p)))
 
 /* 
  * p2roundup --

common/multibyte.h
@@ -102,8 +102,10 @@ typedef	char		RCHAR_T;
 
 #if defined(USE_WIDECHAR) && defined(DEBUG)
 #define MEMCPY			wmemcpy
+#define MEMMOVE			wmemmove
 #else
 #define MEMCPY(p, t, len)	memcpy(p, t, (len) * sizeof(CHAR_T))
+#define MEMMOVE(p, t, len)	memmove(p, t, (len) * sizeof(CHAR_T))
 #endif
 
 #define MEMCMP(to, from, n) 						    \

vi/v_increment.c
@@ -175,7 +175,7 @@ nonum:			msgq(sp, M_ERR, "181|Cursor not in a number");
 	 */
 	GET_SPACE_RETW(sp, bp, blen, len + 50);
 	if (end == len) {
-		MEMMOVEW(bp, &p[beg], wlen);
+		MEMMOVE(bp, &p[beg], wlen);
 		bp[wlen] = '\0';
 		t = bp;
 	} else
@@ -227,9 +227,9 @@ nonum:			msgq(sp, M_ERR, "181|Cursor not in a number");
 	}
 
 	/* Build the new line. */
-	MEMMOVEW(bp, p, beg);
-	MEMMOVEW(bp + beg, nbuf, nlen);
-	MEMMOVEW(bp + beg + nlen, p + end, len - beg - (end - beg));
+	MEMMOVE(bp, p, beg);
+	MEMMOVE(bp + beg, nbuf, nlen);
+	MEMMOVE(bp + beg + nlen, p + end, len - beg - (end - beg));
 	len = beg + nlen + (len - beg - (end - beg));
 
 	nret = NUM_OK;

vi/v_itxt.c
@@ -340,7 +340,7 @@ v_change(SCR *sp, VICMD *vp)
 		if (db_get(sp, vp->m_start.lno, DBG_FATAL, &p, &len))
 			return (1);
 		GET_SPACE_RETW(sp, bp, blen, vp->m_start.cno);
-		MEMMOVEW(bp, p, vp->m_start.cno);
+		MEMMOVE(bp, p, vp->m_start.cno);
 	} else
 		bp = NULL;
 

commit ddf6e5755574931ec6f6766c72259a60327466ca

Fix a bug introduced by 3a0ec51.

common/log.c
@@ -277,7 +277,7 @@ log_line(
 	    len * sizeof(CHAR_T) + CHAR_T_OFFSET);
 	ep->l_lp[0] = action;
 	memmove(ep->l_lp + sizeof(u_char), &lno, sizeof(recno_t));
-	memmove(ep->l_lp + CHAR_T_OFFSET, lp, len);
+	memmove(ep->l_lp + CHAR_T_OFFSET, lp, len * sizeof(CHAR_T));
 
 	lcur = ep->l_cur;
 	key.data = &lcur;