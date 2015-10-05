9744df18a9b6c0584d3127cc1eb3c12b911b0367
Marc Simpson <marc@0branch.com>
2015-10-05 21:36:21 +0100

    Fix incompatible behaviour of T.
    Closes: #35

--- vi/v_ch.c
+++ vi/v_ch.c
@@ -10,7 +10,7 @@
 #include "config.h"
 
 #ifndef lint
-static const char sccsid[] = "$Id: v_ch.c,v 10.11 2011/12/02 19:49:50 zy Exp $";
+static const char sccsid[] = "$Id: v_ch.c,v 10.12 2015/10/09 21:48:57 marc Exp $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -195,6 +195,15 @@ v_chT(SCR *sp, VICMD *vp)
 {
 	if (v_chF(sp, vp))
 		return (1);
+
+	/*
+	 * Check whether the matching character is to the immediate left
+	 * of the original cursor position, offset adjusted for a motion
+	 * command.  If so, no movement is required.
+	 */
+	if (vp->m_start.cno == vp->m_stop.cno) {
+                return (1);
+	}
 
 	/*
 	 * v_chF places the cursor on the character, where the 'T'
