4a7b158f2b4eb063f20968be578865e6f4cdccde
Emir Sarı <bitigchi@me.com>
2020-02-27 22:56:13 +0300

    Populate tr_TR.UTF-8.owner file

--- common/common.h
+++ common/common.h
@@ -9,7 +9,7 @@
  *	$Id: common.h,v 10.23 2017/11/14 00:34:44 zy Exp $
  */
 
-#include <db.h>		/* Only include db1. */
+#include "/usr/include/db.h"	/* Only include db1. */
 #include <regex.h>		/* May refer to the bundled regex. */
 
 /*
