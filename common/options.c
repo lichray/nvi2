d0fa99ffe63b3552c067a0e048221c8ff1b843b1
Laurent <laurent.chardon@gmail.com>
2023-11-16 07:34:55 -0500

    Fix undefined string comparison

--- common/options.c
+++ common/options.c
@@ -317,7 +317,7 @@ opts_init(SCR *sp, int *oargs)
 	/* Set numeric and string default values. */
 #define	OI(indx, str) do {						\
 	a.len = STRLEN(str);						\
-	if ((CHAR_T*)str != b2)	  /* GCC puts strings in text-space. */	\
+	if (STRCMP((CHAR_T*)str, b2))					\
 		(void)MEMCPY(b2, str, a.len+1);				\
 	if (opts_set(sp, argv, NULL)) {					\
 		 optindx = indx;					\
