b44bc209ee6781901a348ccf75f019f34988452e
Job Snijders <job@sobornost.net>
2024-04-25 01:19:19 +0000

    In ruler show the current line number as a percentage of the total lines
    Modeled after control-G, will only show the percentage if the last
    line can be determined.
    
    From OpenBSD https://github.com/openbsd/src/commit/b254483d64afb832597affd199aa63d6d99b70a3
    and https://github.com/openbsd/src/commit/39e743a585bfb0027efb6a4bb6f3b6b02f8ed1e3

--- vi/vs_refresh.c
+++ vi/vs_refresh.c
@@ -774,7 +774,8 @@ vs_modeline(SCR *sp)
 	size_t cols, curcol, curlen, endpoint, len, midpoint;
 	const char *t = NULL;
 	int ellipsis;
-	char buf[20];
+	char buf[30];
+	recno_t last;
 
 	gp = sp->gp;
 
@@ -846,8 +847,14 @@ vs_modeline(SCR *sp)
 	cols = sp->cols - 1;
 	if (O_ISSET(sp, O_RULER)) {
 		vs_column(sp, &curcol);
-		len = snprintf(buf, sizeof(buf), "%lu,%lu",
-		    (u_long)sp->lno, (u_long)(curcol + 1));
+
+		if (db_last(sp, &last) || last == 0)
+			len = snprintf(buf, sizeof(buf), "%lu,%zu",
+			    (u_long)sp->lno, curcol + 1);
+		else
+			len = snprintf(buf, sizeof(buf), "%lu,%zu %lu%%",
+			    (u_long)sp->lno, curcol + 1,
+			    (u_long)(sp->lno * 100) / last);
 
 		midpoint = (cols - ((len + 1) / 2)) / 2;
 		if (curlen < midpoint) {
